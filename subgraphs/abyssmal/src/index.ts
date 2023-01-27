574dd0d9ce578a43baf8078a81f26cb097418eb7
Chad Lynch <chyad@hotmail.com>
2023-01-26 23:19:10 -0500

    feat: Add mint cost udpated event, other

--- subgraphs/abyssmal/src/index.ts
+++ subgraphs/abyssmal/src/index.ts
@@ -1,15 +1,40 @@
-import { Approvals } from "../generated/schema";
-import { Approval } from "../generated/Abyssmal/Abyssmal";
+import { ApprovalTx, ApprovalForAllTx, MintCostUpdatedTx } from "../generated/schema";
+import { Approval, ApprovalForAll, MintCostUpdated } from "../generated/Abyssmal/Abyssmal";
 
 export function handleApproval(event: Approval): void {
   const logIndex = event.logIndex;
   const transactionHash = event.transaction.hash;
 
   const approvalId = `${transactionHash}-${logIndex.toString()}`;
-  const approval = new Approvals(approvalId);
+  const approvalTx = new ApprovalTx(approvalId);
 
-  approval.owner = event.params.owner.toHex();
-  approval.approved = event.params.approved.toHex();
-  approval.tokenId = event.params.tokenId.toI32();
-  approval.save();
+  approvalTx.owner = event.params.owner.toHex();
+  approvalTx.approved = event.params.approved.toHex();
+  approvalTx.tokenId = event.params.tokenId.toI32();
+  approvalTx.save();
+}
+
+export function handleApprovalForAll(event: ApprovalForAll): void {
+  const logIndex = event.logIndex;
+  const transactionHash = event.transaction.hash;
+
+  const id = `${transactionHash}-${logIndex.toString()}`;
+  const approvalForAllTx = new ApprovalForAllTx(id);
+
+  approvalForAllTx.owner = event.params.owner.toHex();
+  approvalForAllTx.operator = event.params.operator.toHex();
+  approvalForAllTx.approved = event.params.approved;
+  approvalForAllTx.save();
+}
+
+export function handleMintCostUpdated(event: MintCostUpdated): void {
+  const logIndex = event.logIndex;
+  const transactionHash = event.transaction.hash;
+
+  const id = `${transactionHash}-${logIndex.toString()}`;
+  const mintCostUpdatedTx = new MintCostUpdatedTx(id);
+
+  mintCostUpdatedTx.oldMintCost = event.params.oldMintCost;
+  mintCostUpdatedTx.newMintCost = event.params.newMintCost;
+  mintCostUpdatedTx.save();
 }
