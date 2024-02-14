604a32a94f808b4c4e92999aa164282f45f55c5a
Jyotirmoy Barman <jyotirmoydotdev@gmail.com>
2024-02-15 00:15:11 +0530

    feat: Chamber subgraph added

---
+++ subgraphs/chamber/tests/chamber.test.ts
@@ -0,0 +1,193 @@
+import { describe, test } from "matchstick-as";
+
+import {
+  ChangedGuard,
+  Demoted,
+  Initialized,
+  Promoted,
+  ProposalApproved,
+  // ProposalCreated,
+  ProposalExecuted,
+  ReceivedEther,
+  ReceivedFallback,
+} from "../generated/templates/Chamber/Chamber";
+
+import {
+  handleChangedGuard,
+  handleDemoted,
+  handleInitialized,
+  handlePromoted,
+  handleProposalApproved,
+  // handleProposalCreated,
+  handleProposalExecuted,
+  handleReceivedEther,
+  handleReceivedFallback,
+} from "../src/chamber";
+
+import {
+  createChangedGuardEvent,
+  createDemotedEvent,
+  createInitializedEvent,
+  createPromotedEvent,
+  createProposalApprovedEvent,
+  // createProposalCreatedEvent,
+  createProposalExecutedEvent,
+  createReceivedEtherEvent,
+  createReceivedFallbackEvent,
+} from "./helpers/utils";
+
+import {
+  expectChangedGuardAdded,
+  expectDemotedAdded,
+  expectInitializedAdded,
+  expectPromotedAdded,
+  expectProposalApprovedAdded,
+  // expectProposalCreatedAdded,
+  expectProposalExecutedAdded,
+  expectReceivedEtherAdded,
+  expectReceivedFallbackAdded,
+} from "./helpers/assertions";
+
+describe("Chamber Test", () => {
+  describe("Test ChangedGuard", () => {
+    const createMockChangedGuardEvent = (
+      guard: string = "0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf"
+    ): ChangedGuard => {
+      const event = createChangedGuardEvent(guard);
+      return event;
+    };
+    test("Handel ChangedGuard Tx", () => {
+      const event = createMockChangedGuardEvent();
+      handleChangedGuard(event);
+      expectChangedGuardAdded(event);
+    });
+  });
+
+  describe("Test Demoted", () => {
+    const createMockDemotedEvent = (
+      demoter: string = "0x8e1aa1674d9fc9f0dca4a4d31db85e65d216666c",
+      amt: i32 = 100,
+      tokenId: i32 = 1
+    ): Demoted => {
+      const event = createDemotedEvent(demoter, amt, tokenId);
+      return event;
+    };
+    test("Handel Demoted Tx", () => {
+      const event = createMockDemotedEvent();
+      handleDemoted(event);
+      expectDemotedAdded(event);
+    });
+  });
+
+  describe("Test Initialized", () => {
+    const createMockInitializedEvent = (version: i32 = 1): Initialized => {
+      const event = createInitializedEvent(version);
+      return event;
+    };
+    test("Handel Initialized Tx", () => {
+      const event = createMockInitializedEvent();
+      handleInitialized(event);
+      expectInitializedAdded(event);
+    });
+  });
+
+  describe("Test Promoted", () => {
+    const createMockPromotedEvent = (
+      promoter: string = "0x8e1aa1674d9fc9f0dca4a4d31db85e65d216666c",
+      amt: i32 = 100,
+      tokenId: i32 = 1
+    ): Promoted => {
+      const event = createPromotedEvent(promoter, amt, tokenId);
+      return event;
+    };
+    test("Handel Promoted Tx", () => {
+      const event = createMockPromotedEvent();
+      handlePromoted(event);
+      expectPromotedAdded(event);
+    });
+  });
+
+  describe("Test ProposalApproved", () => {
+    const createMockProposalApprovedEvent = (
+      proposalId: i32 = 1,
+      tokenId: i32 = 1,
+      approvals: i32 = 1
+    ): ProposalApproved => {
+      const event = createProposalApprovedEvent(proposalId, tokenId, approvals);
+      return event;
+    };
+    test("Handel ProposalApproved Tx", () => {
+      const event = createMockProposalApprovedEvent();
+      handleProposalApproved(event);
+      expectProposalApprovedAdded(event);
+    });
+  });
+
+  // describe ("Test ProposalCreated", () => {
+  //     const createMockProposalCreatedEvent = (
+  //         proposalId: i32 = 1,
+  //         target: string[] = ['0x2e0049b05217290087BA613290BaCC761d7adD04','0x8E1aA1674D9Fc9f0dca4a4D31db85E65D216666c'] ,
+  //         value: i32[] = [1,1],
+  //         data: string[] = ['',''],
+  //         voters: i32[] = [1,2,3,4,5],
+  //         nonce: i32 = 1,
+  //     ) : ProposalCreated => {
+  //         const event = createProposalCreatedEvent(
+  //             proposalId,
+  //             target,
+  //             value,
+  //             data,
+  //             voters,
+  //             nonce,
+  //         );
+  //         return event;
+  //     };
+  //     test ("Handel ProposalCreated Tx", () => {
+  //         const event = createMockProposalCreatedEvent();
+  //         handleProposalCreated(event);
+  //         expectProposalCreatedAdded(event);
+  //     })
+  // })
+
+  describe("Test ProposalExecuted", () => {
+    const createMockProposalExecutedEvent = (proposalId: i32 = 1): ProposalExecuted => {
+      const event = createProposalExecutedEvent(proposalId);
+      return event;
+    };
+    test("Handel ProposalExecuted Tx", () => {
+      const event = createMockProposalExecutedEvent();
+      handleProposalExecuted(event);
+      expectProposalExecutedAdded(event);
+    });
+  });
+
+  describe("Test ReceivedEther", () => {
+    const createMockReceivedEtherEvent = (
+      sender: string = "0x8E1aA1674D9Fc9f0dca4a4D31db85E65D216666c",
+      value: i32 = 1000
+    ): ReceivedEther => {
+      const event = createReceivedEtherEvent(sender, value);
+      return event;
+    };
+    test("Handel ReceivedEther Tx", () => {
+      const event = createMockReceivedEtherEvent();
+      handleReceivedEther(event);
+      expectReceivedEtherAdded(event);
+    });
+  });
+
+  describe("Test ReceivedFallback", () => {
+    const createMockReceivedFallbackEvent = (
+      sender: string = "0x8E1aA1674D9Fc9f0dca4a4D31db85E65D216666c",
+      value: i32 = 1000
+    ): ReceivedFallback => {
+      const event = createReceivedFallbackEvent(sender, value);
+      return event;
+    };
+    test("Handel ReceivedFallback Tx", () => {
+      const event = createMockReceivedFallbackEvent();
+      handleReceivedFallback(event);
+      expectReceivedFallbackAdded(event);
+    });
+  });
+});
---
+++ subgraphs/chamber/tests/helpers/utils.ts
@@ -0,0 +1,231 @@
+import { Address, BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts";
+import { newMockEvent } from "matchstick-as";
+
+import { ChamberDeployed, OwnershipTransferred } from "../../generated/Registry/Registry";
+
+import {
+  ChangedGuard,
+  Demoted,
+  Initialized,
+  Promoted,
+  ProposalApproved,
+  ProposalCreated,
+  ProposalExecuted,
+  ReceivedEther,
+  ReceivedFallback,
+} from "../../generated/templates/Chamber/Chamber";
+
+const addressEventParam = (key: string, value: string): ethereum.EventParam =>
+  new ethereum.EventParam(key, ethereum.Value.fromAddress(Address.fromString(value)));
+
+const uintEventParam = (key: string, value: i32): ethereum.EventParam =>
+  new ethereum.EventParam(key, ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(value)));
+
+export function createChamberDeployedEvent(
+  chamber: string,
+  serial: i32,
+  deployer: string,
+  memberToken: string,
+  govToken: string
+): ChamberDeployed {
+  const mockEvent = newMockEvent();
+  const chamberDeployedEvent = new ChamberDeployed(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    [],
+    mockEvent.receipt
+  );
+  chamberDeployedEvent.parameters.push(addressEventParam("chamber", chamber));
+  chamberDeployedEvent.parameters.push(uintEventParam("serial", serial));
+  chamberDeployedEvent.parameters.push(addressEventParam("deployer", deployer));
+  chamberDeployedEvent.parameters.push(addressEventParam("memberToken", memberToken));
+  chamberDeployedEvent.parameters.push(addressEventParam("govToken", govToken));
+  return chamberDeployedEvent;
+}
+
+export function createOwnershipTransferredEvent(previousOwner: string, newOwner: string): OwnershipTransferred {
+  const mockEvent = newMockEvent();
+  const ownershipTransferredEvent = new OwnershipTransferred(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    [],
+    mockEvent.receipt
+  );
+  ownershipTransferredEvent.parameters.push(addressEventParam("previousOwner", previousOwner));
+  ownershipTransferredEvent.parameters.push(addressEventParam("newOwner", newOwner));
+  return ownershipTransferredEvent;
+}
+
+export function createChangedGuardEvent(guard: string): ChangedGuard {
+  const mockEvent = newMockEvent();
+  const changedGuardEvent = new ChangedGuard(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    [],
+    mockEvent.receipt
+  );
+  changedGuardEvent.parameters.push(addressEventParam("guard", guard));
+  return changedGuardEvent;
+}
+
+export function createDemotedEvent(demoter: string, amt: i32, tokenId: i32): Demoted {
+  const mockEvent = newMockEvent();
+  const demotedEvent = new Demoted(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    [],
+    mockEvent.receipt
+  );
+  demotedEvent.parameters.push(addressEventParam("demoter", demoter));
+  demotedEvent.parameters.push(uintEventParam("amt", amt));
+  demotedEvent.parameters.push(uintEventParam("tokenId", tokenId));
+  return demotedEvent;
+}
+
+export function createInitializedEvent(version: i32): Initialized {
+  const mockEvent = newMockEvent();
+  const initializedEvent = new Initialized(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    [],
+    mockEvent.receipt
+  );
+  initializedEvent.parameters.push(uintEventParam("version", version));
+  return initializedEvent;
+}
+
+export function createPromotedEvent(promoter: string, amt: i32, tokenId: i32): Promoted {
+  const mockEvent = newMockEvent();
+  const promotedEvent = new Promoted(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    [],
+    mockEvent.receipt
+  );
+  promotedEvent.parameters.push(addressEventParam("demoter", promoter));
+  promotedEvent.parameters.push(uintEventParam("amt", amt));
+  promotedEvent.parameters.push(uintEventParam("tokenId", tokenId));
+  return promotedEvent;
+}
+
+export function createProposalApprovedEvent(proposalId: i32, tokenId: i32, approvals: i32): ProposalApproved {
+  const mockEvent = newMockEvent();
+  const proposalApprovedEvent = new ProposalApproved(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    [],
+    mockEvent.receipt
+  );
+  proposalApprovedEvent.parameters.push(uintEventParam("proposalId", proposalId));
+  proposalApprovedEvent.parameters.push(uintEventParam("tokenId", tokenId));
+  proposalApprovedEvent.parameters.push(uintEventParam("approvals", approvals));
+  return proposalApprovedEvent;
+}
+
+export function createProposalCreatedEvent(
+  proposalId: i32,
+  target: string[],
+  value: i32[],
+  data: string[],
+  voters: i32[],
+  nonce: i32
+): ProposalCreated {
+  const mockEvent = newMockEvent();
+  const proposalCreatedEvent = new ProposalCreated(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    [],
+    mockEvent.receipt
+  );
+  proposalCreatedEvent.parameters.push(uintEventParam("proposalId", proposalId));
+  proposalCreatedEvent.parameters.push(
+    new ethereum.EventParam("target", ethereum.Value.fromAddressArray(changetype<Bytes[]>(target)))
+  );
+  proposalCreatedEvent.parameters.push(new ethereum.EventParam("value", ethereum.Value.fromI32Array(value)));
+  proposalCreatedEvent.parameters.push(new ethereum.EventParam("data", ethereum.Value.fromStringArray(data)));
+  proposalCreatedEvent.parameters.push(new ethereum.EventParam("voters", ethereum.Value.fromI32Array(voters)));
+  proposalCreatedEvent.parameters.push(uintEventParam("nonce", nonce));
+  return proposalCreatedEvent;
+}
+
+export function createProposalExecutedEvent(proposalId: i32): ProposalExecuted {
+  const mockEvent = newMockEvent();
+  const proposalExecutedEvent = new ProposalExecuted(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    [],
+    mockEvent.receipt
+  );
+  proposalExecutedEvent.parameters.push(uintEventParam("proposalId", proposalId));
+  return proposalExecutedEvent;
+}
+
+export function createReceivedEtherEvent(sender: string, value: i32): ReceivedEther {
+  const mockEvent = newMockEvent();
+  const receivedEtherEvent = new ReceivedEther(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    [],
+    mockEvent.receipt
+  );
+  receivedEtherEvent.parameters.push(addressEventParam("sender", sender));
+  receivedEtherEvent.parameters.push(uintEventParam("value", value));
+  return receivedEtherEvent;
+}
+
+export function createReceivedFallbackEvent(sender: string, value: i32): ReceivedFallback {
+  const mockEvent = newMockEvent();
+  const receivedFallbackEvent = new ReceivedFallback(
+    mockEvent.address,
+    mockEvent.logIndex,
+    mockEvent.transactionLogIndex,
+    mockEvent.logType,
+    mockEvent.block,
+    mockEvent.transaction,
+    [],
+    mockEvent.receipt
+  );
+  receivedFallbackEvent.parameters.push(addressEventParam("sender", sender));
+  receivedFallbackEvent.parameters.push(uintEventParam("value", value));
+  return receivedFallbackEvent;
+}
